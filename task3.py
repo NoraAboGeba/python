f3fdfdb115fc02b8d76c7942771edaa9b847047c
nora <noramahmoudahmed9@gmail.com>
2018-02-04 21:58:41 +0200

    third commit

---
+++ task3.py
@@ -0,0 +1,16 @@
+#stars shift
+
+
+num=int(input("Enter a number: "))
+for i in range(num+1):
+    for z in range(num-i):
+            
+        print("  ",end='')
+    for y in range(i):
+            
+        print("* ",end='')
+        
+        
+        
+    print("\n")
+  
